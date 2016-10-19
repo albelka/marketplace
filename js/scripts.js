ce0dc6b14a6dafefac5e91df75ea86fb9c145ae9
Anne Belka and Brian Pritt <brian@>
2016-10-19 16:42:13 -0700

    add a receipt section(form disappears) and some basic styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,22 @@
 $(document).ready(function(){
-  $("#blank-form form").submit(function(event){
+  $("#blankform form").submit(function(event){
+
     var firstnameInput = $("input#first-name").val();
     var lastnameInput = $("input#last-name").val();
     var streetaddInput = $("input#street-add").val();
+    var cityInput = $("input#city").val();
     var zipInput = $("input#zip-code").val();
     var stateInput= $("#state").val();
-  })
-})
+
+    $(".first-name").text(firstnameInput);
+    $(".last-name").text(lastnameInput);
+    $(".street-add").text(streetaddInput);
+    $(".city").text(cityInput);
+    $(".state").text(stateInput);
+    $(".zip-code").text(zipInput);
+    $("#blankform").remove("div #blankform");
+    $(".receipt").show();
+    event.preventDefault();
+  });
+
+});
